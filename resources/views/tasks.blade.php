44f95397c090b3f0ca6497990df834bb3e2c5a3d
Yousef Zimawi <ymwsy2568@gmil.com>
2025-12-22 15:05:50 +0300

    delete vite

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tasks</title>
-     @vite([
-        'resources/css/freomwrok.css',
-        'resources/css/dashboard.css',
-        'resources/css/app.css',
-        ])
+    
           <link rel="stylesheet" href="{{ asset('css/freomwrok.css') }}">
         <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">    
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">  
